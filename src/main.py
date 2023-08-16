562f6ddf17bea0008efb5559c9815c19aceaee44
kolyazin <kolyazin.se@gmai.com>
2023-08-16 18:17:40 +0300

    add States

--- src/main.py
+++ src/main.py
@@ -5,9 +5,10 @@ from os import getenv
 from sys import exit
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
 from aiogram.utils import executor
-from aiogram.dispatcher.filters import Text
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.storage import FSMContext
 
 load_dotenv()
 # Bot token can be obtained via https://t.me/BotFather
@@ -18,58 +19,57 @@ if not API_TOKEN:
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
-
+storage = MemoryStorage()
 # All handlers should be attached to the Router (or Dispatcher)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
+class XOStates(StatesGroup):
+    wait_next_step = State()
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
+    # This handler will be called when user sends `/start` or `/help` command
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
-buttons = []
-steps = []
-
 @dp.message_handler(commands='xo')
-async def kbd(message: types.Message):
-#    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    global buttons
-    global steps
-    buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    steps = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+async def show_time(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    data['buttons'] = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    data['steps'] = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    await state.update_data(data)
     keyboard = types.ReplyKeyboardMarkup()
-    keyboard.add(*buttons)
-    await message.answer("Крестики-нолики", reply_markup=keyboard)
+    keyboard.add(*data['buttons'])
+    await message.answer(f'Крестики-нолики\nПривет, {message.from_user.first_name}\nТвой ход', reply_markup=keyboard)
+    await XOStates.wait_next_step.set()
 
-@dp.message_handler(lambda message: message.text in steps)
-async def one(message: types.Message):
-    global buttons
-    global steps
+@dp.message_handler(state=XOStates.wait_next_step)
+async def play(message: types.Message, state: FSMContext):
     mess = 'Твой ход'
     keyboard = types.ReplyKeyboardMarkup()
 
+    data = await state.get_data()
     # крестик   
-    buttons[buttons.index(message.text)] = '\u274C'
-    steps.pop(steps.index(message.text))
+    data['buttons'][data['buttons'].index(message.text)] = '\u274C'
+    data['steps'].pop(data['steps'].index(message.text))
 
-    if not win(buttons=buttons):
-        if steps:
-            bot_step = random.choice(steps)
+    if not win(buttons=data['buttons']):
+        if data['steps']:
+            bot_step = random.choice(data['steps'])
             # нолик
-            buttons[buttons.index(bot_step)] = '\u2B55'
-            steps.remove(bot_step)
-            if win(buttons=buttons):
+            data['buttons'][data['buttons'].index(bot_step)] = '\u2B55'
+            data['steps'].remove(bot_step)
+            if win(buttons=data['buttons']):
                 mess='Я выиграл\nКонец игры'
-                steps = []
+                await state.reset_state()
         else:
             mess = 'Ничья\nКонец игры'
+            await state.reset_state()
     else:
         mess = 'Ты выиграл\nКонец игры'
-        steps = []
+        await state.reset_state()
 
-    keyboard.add(*buttons)
+    await state.update_data(data)
+    keyboard.add(*data['buttons'])
     await message.answer(mess, reply_markup=keyboard)
 
 
@@ -79,8 +79,8 @@ async def echo(message: types.Message):
     # await bot.send_message(message.chat.id, message.text)
     await message.answer(message.text)
 
-def win(buttons: buttons):
-    print(buttons)
+def win(buttons: []):
+#    print(buttons)
     if buttons[0] == buttons[1] and buttons[0] == buttons[2]:
         return True
     if buttons[3] == buttons[4] and buttons[3] == buttons[5]:
@@ -98,7 +98,6 @@ def win(buttons: buttons):
     if buttons[2] == buttons[4] and buttons[2] == buttons[6]:
         return True
     return False
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
